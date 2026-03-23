22f6e8df516b36319d7c83170da75cf2526a85f6
LucaOrlandi98 <orlandiluca00@gmail.com>
2026-03-23 22:29:37 +0100

    ll

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({
-    plugins: [react()],
+var repoBase = "/PANDA-PROJECT/";
+export default defineConfig(function (_a) {
+    var mode = _a.mode;
+    return ({
+        base: mode === "production" ? repoBase : "/",
+        plugins: [react()],
+    });
 });
